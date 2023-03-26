5dd817e395166b8d9606b9c7d5334408d5a9b989
Sven Swers <swers_sven@hotmail.com>
2023-03-26 21:44:04 +0200

    Agregado funcionalidad de traducción y creación de componente para 'Home'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Tube_Tracker';
+
+  constructor(public tranlate: TranslateService) {
+    // Definimos lenguajes traducción y por defecto.
+    this.tranlate.addLangs(['es', 'en']);
+    this.tranlate.setDefaultLang('es');
+    this.tranlate.use('en');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+// Librerías traducción.
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HomeComponent } from './components/home/home.component';
+
+// Función para cargar archivo de traducción del proyecto.
+export function HttpLoaderFactory(http: HttpClient){
+  return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
